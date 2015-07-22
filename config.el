57518d83d00f09fe83edd1906d25bbd0e60ede61
Pieter <pieterv.sorano@gmail.com>
2015-07-23 00:11:04 +0200

    Added yasnippet config and some snippets for lua

--- config.el
+++ config.el
@@ -48,7 +48,7 @@
                          ("SC" . "http://joseito.republika.pl/sunrise-commander/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
-(setq package-list '(ace-jump-mode ecb goto-chg undo-tree expand-region f dash s flymake-lua flymake-python-pyflakes flymake-easy flymake-yaml flymake-easy goto-chg iy-go-to-char jedi python-environment deferred auto-complete popup epc ctable concurrent deferred lua-mode magit git-rebase-mode git-commit-mode multiple-cursors nurumacs popup projectile pkg-info epl dash s pymacs python-environment deferred s sr-speedbar ssh sunrise-commander undo-tree yaml-mode powerline solarized-theme markdown-mode))
+(setq package-list '(ace-jump-mode ecb goto-chg undo-tree expand-region f dash s flymake-lua flymake-python-pyflakes flymake-easy flymake-yaml flymake-easy goto-chg iy-go-to-char jedi python-environment deferred auto-complete popup epc ctable concurrent deferred lua-mode magit git-rebase-mode git-commit-mode multiple-cursors nurumacs popup projectile pkg-info epl dash s pymacs python-environment deferred s sr-speedbar ssh sunrise-commander undo-tree yaml-mode powerline solarized-theme markdown-mode helm helm-pydoc helm-projectile helm-spotify))
 
 ;; refresh package archive
 (unless package-archive-contents
@@ -72,6 +72,12 @@
 (setq ecb-options-version "2.40")
 (setq ecb-tip-of-the-day nil)
 
+(setq org-replace-disputed-keys t)
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 (setq org-src-fontify-natively t)
 
 (org-babel-do-load-languages
@@ -84,6 +90,29 @@
 (setq org-confirm-babel-evaluate nil)
 
 (setq org-babel-python-command "python2")
+
+(require 'helm-config)
+(helm-mode 1)
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
+
+(when (executable-find "curl")
+    (setq helm-google-suggest-use-curl-p t))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h g") 'helm-google-suggest)
+(define-key helm-map (kbd "M-y") 'helm-show-kill-ring)
 
 (load-theme 'solarized-dark t)
 
@@ -136,6 +165,7 @@
 (global-set-key (kbd "C-c n") 'senator-next-tag)
 (global-set-key (kbd "C-c p") 'senator-previous-tag)
 (global-set-key (kbd "C-D") 'python-docstring)
+(global-set-key (kbd "C-c h p") 'helm-pydoc)
 
 (autoload
   'ace-jump-mode
@@ -175,3 +205,5 @@
 (global-set-key (kbd "M-w") 'custom-copy-line-or-region) ; copy
 
 (global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+
+(define-key helm-map (kbd "C-c h p")  'helm-projechtile)
