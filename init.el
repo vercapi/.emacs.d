e5aa547f99d0a00e5fddd8cdf113b57a189bb110
Pieter <pieterv.sorano@gmail.com>
2016-10-12 13:37:20 +0200

    Changes after upgrading to emacs 25.1

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (put 'dired-find-alternate-file 'disabled nil)
+(put 'upcase-region 'disabled nil)
