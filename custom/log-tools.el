c93289e52617f0a3f1982c25d888b2058306ae01
Pieter <pieterv.sorano@gmail.com>
2017-10-05 11:44:16 +0200

    Changes after update of emacs

--- custom/log-tools.el
+++ custom/log-tools.el
@@ -3,6 +3,7 @@
 
 ;;; Code:
 
+
 (defvar log-tools-servers)
 (defvar log-tools-url)
 
@@ -11,6 +12,12 @@
 (puthash "utastwlsprod6" (vector "utastwlsprod6a" "utastwlsprod6b") log-tools-servers)
 (puthash "utastwlsprod7" (vector "utastwlsprod7a" "utastwlsprod7b") log-tools-servers)
 (puthash "utastwlsprod8" (vector "utastwlsprod8a" "utastwlsprod8b") log-tools-servers)
+
+(defun log-tools-open-file-wls-kbvb (server-name  managed-server-name)
+  "Effectively open files on machine with SERVER-NAME and for managed server with MANAGED-SERVER-NAME."
+  (setq log-tools-url (concat "/ssh:" server-name ":/opt/app/oracle/product/12.2.1/user_projects/domains/urbsfa_prod/servers/" managed-server-name "/logs/" managed-server-name "-diagnostic.log"))
+  (find-file-noselect log-tools-url)
+  )
 
 (mapc
  (lambda (x)
@@ -23,14 +30,5 @@
     (gethash x log-tools-servers))
    )
  (hash-table-keys log-tools-servers))
-
-
-
-(defun log-tools-open-file-wls-kbvb (server-name  managed-server-name)
-  "Effectively open files on machine with SERVER-NAME and for managed server with MANAGED-SERVER-NAME."
-  (setq log-tools-url (concat "/ssh:" server-name ":/opt/app/oracle/product/12.2.1/user_projects/domains/urbsfa_prod/servers/" managed-server-name "/logs/" managed-server-name "-diagnostic.log"))
-  (find-file-noselect log-tools-url)
-  )
-
 
 ;;; log-tools.el ends here
