9ea1d62a05bd1f18b09131df8513d5d5fb5df6ca
Pieter <pieterv.sorano@gmail.com>
2018-06-23 21:44:21 +0200

    Moved the custom pve font manipulation to the org file.
    Goal is to move all code in the org file or in packages

--- custom/emacs-tools.el
+++ custom/emacs-tools.el
@@ -3,24 +3,6 @@
 
 ;;; Code:
 
-(defun pve-small-fonts ()
-  "Set font to work on desktop screens."
-  (interactive)
-  (pve-set-fonts 100)
-  )
-
-(defun pve-big-fonts ()
-  "Set font for use on laptop screens."
-   (interactive)
-  (pve-set-fonts 140)
-  )
-
-(defun pve-set-fonts (font-height)
-  "Set size of fonts to FONT-HEIGHT, and only the size of the fonts."
-  (set-face-attribute 'default nil :height font-height)
-  (set-face-attribute 'mode-line nil :height font-height)
-  )
-
 (defun pve-copy-full-file-name-to-clipboard ()
   "Copy the current buffer file name and path to the clipboard."
   (interactive)
