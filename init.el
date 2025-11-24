df7af950c471671c107bd76d9f7307b524020d41
vercapi <pieter.vercammen@cepa.be>
2025-11-24 22:27:10 +0100

    Fixed issues with fonts
    automatically install needed font

--- init.el
+++ init.el
@@ -13,15 +13,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(which-key vterm visual-regexp undo-tree twittering-mode treemacs-projectile sunrise-commander speed-type salt-mode rfc-mode rainbow-delimiters quelpa-use-package pyvenv python-mode powerline pocket-mode plantuml-mode pdf-tools pcap-mode parse-csv orgit org-present org-gcal org-download org-bullets openapi-yaml-mode ob-restclient ob-async nord-theme magit-gitflow lyrics lua-mode lsp-ui lsp-java loccur lastpass java-snippets indium impatient-mode helm-xref helm-swoop helm-projectile helm-org helm-icons haskell-mode goto-last-change flycheck flucui-themes fish-completion eyebrowse expand-region excorporate eros dockerfile-mode docker dired-quick-sort diminish company-tern company-restclient company-lsp company-box clipmon cider bm beacon avy-zap angular-snippets all-the-icons ace-jump-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "ProggyVector" :foundry "PfEd" :slant normal :weight normal :height 120 :width normal))))
- '(minimap-font-face ((t (:height 40 :family "ProggyVector"))))
- '(mode-line ((t (:family "ProggyVector" :foundry "PfEd" :slant normal :weight normal :height 120 :width normal))))
- '(treemacs-directory-face ((t :inherit default)))
- '(treemacs-git-untracked-face ((t :inherit default))))
+ '(package-selected-packages nil))
